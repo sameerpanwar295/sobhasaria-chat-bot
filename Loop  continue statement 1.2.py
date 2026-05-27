9823835f92020b760d7d5d4306be6e3490458818
sameergit config --global user.name sameer <sameersainisameer997@gmail.com>
2026-05-27 17:17:31 +0530

    my 5th project

---
+++ Loop  continue statement 1.2.py
@@ -0,0 +1,5 @@
+fruits = ["mango","papaya","orange","grapes"]
+for x in fruits:
+    if x == "papaya":
+        continue
+    print(x)
---
+++ nested loop1.py
@@ -0,0 +1,6 @@
+adj = ["blue", "small", "tasty"]
+fruits = ["apple", "banana", "cherry"]
+
+for x in adj:
+  for y in fruits:
+    print(x, y)
