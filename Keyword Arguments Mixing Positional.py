92715108677dfe823a3a09bf5de27b017ebebe0b
sameergit config --global user.name sameer <sameersainisameer997@gmail.com>
2026-06-03 16:05:45 +0530

    my 8th project

---
+++ Keyword Arguments Mixing Positional.py
@@ -0,0 +1,4 @@
+def my_function(animal , name , age):
+ print("I have a ",age,"year old",animal,"named",name)
+
+my_function("dog",name="buddy", age=7)
---
+++ make 5 table by loop in multiplication method.py
@@ -0,0 +1,4 @@
+table = int(input("Enter the number"))
+for x in range(1,11):
+    t =  x*input
+    print(t)
